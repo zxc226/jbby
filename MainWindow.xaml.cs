3e8100e4e53a0d5611d5a885299466e29740cbfd
Administrator <Administrator@DESKTOP-60JBMB2>
2022-07-23 11:46:31 +0800

    cxy

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -60,7 +60,22 @@ namespace jbby
         /// <param name="e"></param>
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            
+            //创建＂打开文件＂对话框
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            //设置文件类型过滤
+            dlg.Filter = "Word文档|*.docx|Word文档（95~2007）|*.doc|文本文档|*.txt;";
+            // 调用ShowDialog方法显示＂打开文件＂对话框
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                //获取所选文件名并在FileNameTextBox中显示完整路径
+                string filename = dlg.FileName;
+                wjdx.Content = dlg.OpenFile().Length;
+                int staerindex = dlg.SafeFileName.Trim().Length-1;
+                int endindex = dlg.SafeFileName.IndexOf(".");
+                var wjlxx = dlg.SafeFileName.Substring(endindex, staerindex);
+                wjlx.Content = wjlxx;
+            }
         }
         /// <summary>
         /// 重置
---
+++ Setting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jbby
+{
+    public class Setting
+    {
+        public string StaerFH { get; set; }
+        public string EndFH { get; set; }
+        public string ZTColore { get; set; }
+        public string Type { get; set; }
+        public string JSName { get; set; }
+    }
+}
